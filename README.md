bfcf0260193b4aa3fe649df34e91ae0e969c5504
121ananya <sinhaananya050@gmail.com>
2021-05-03 01:36:59 +0530

    changes

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pagination-Filtering-REACT-Search
+
+### [Live Site](https://anwesha-filter-pagination-demonstration.netlify.app/?page=1)
+
+## Introduction
+
+Its a fully functional Site To Demonstrate Pagination, Filtration, Ranking, Search Operation. Offline-first Progressive Web Apps are faster and more reliable than traditional web pages, and provide an engaging mobile experience. So inorder, to make it more reliable. I have used: 
+```
+npx create-react-app my-app --template cra-template-pwa
+```
+
+It uses concept of advanced React & JavaScript topics, as well as how to use NodeJS, REST.
+
+## To Run The Project
+- npm install
+- sudo npm install --global yarn
+- yarn start
+- For Production Level Run--->npm run build
---
+++ build/precache-manifest.a31776ba1b34e21aea3b8261e0223af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df9f84cc92ef19e9f6b960eda1d0a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3d3b26a2688fc8e6a8c",
+    "url": "/static/css/2.1d10ae30.chunk.css"
+  },
+  {
+    "revision": "4c9c35e88e12fe065439",
+    "url": "/static/css/main.bc59b68d.chunk.css"
+  },
+  {
+    "revision": "a3d3b26a2688fc8e6a8c",
+    "url": "/static/js/2.b99dc346.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b99dc346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9c35e88e12fe065439",
+    "url": "/static/js/main.00de4d44.chunk.js"
+  },
+  {
+    "revision": "38770da8927cb8cd379a",
+    "url": "/static/js/runtime-main.9313ce03.js"
+  }
+]);
